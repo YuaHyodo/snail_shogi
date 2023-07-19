6d06e173542f843dfcd30c7eb68f36aca5c3e0bd
YuaHyodo <66828980+YuaHyodo@users.noreply.github.com>
2023-07-19 15:06:02 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -24,5 +24,5 @@ SOFTWARE.
 
 from distutils.core import setup
 
-setup(name='snail_shogi', version='0.0.3', description='Library of shogi very very slow like a snail',
-         auther='Yua Hyodo', url='https://github.com/YuaHyodo/snail_shogi', packages=['snail_shogi'])
+setup(name='snail_shogi', version='0.0.4', description='Library of shogi very very slow like a snail',
+         author='YuaHyodo', url='https://github.com/YuaHyodo/snail_shogi', packages=['snail_shogi'])
